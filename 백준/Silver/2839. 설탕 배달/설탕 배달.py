2e12f7321577c19ec9ddde61704696df283592e9
choizeus <98094516+choizeus02@users.noreply.github.com>
2025-08-27 01:57:05 +0900

    [Silver IV] Title: 설탕 배달, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,17 @@
+import sys
+
+n = int(sys.stdin.readline().strip())
+
+
+m = 0
+
+while n >= 0:
+    if n % 5 == 0:
+        m += (n // 5)
+        print(m)
+        break
+    else:
+        n -= 3
+        m += 1
+else:
+    print(-1)
