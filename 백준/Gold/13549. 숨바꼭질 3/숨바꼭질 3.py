42a0fdeb20d29407916dedb8f7d608368eebc6b1
choizeus <98094516+choizeus02@users.noreply.github.com>
2025-11-15 15:56:36 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 96 ms, Memory: 37992 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -0,0 +1,31 @@
+import sys
+input=sys.stdin.readline
+from collections import deque
+
+def bfs(i,j):
+    q = deque([i])
+    visited[i] = 1
+    while q:
+        x = q.popleft()
+        if x == j:
+            return
+        for nx in (2*x, x-1, x+1):
+            if nx <0 or nx > MAX:
+                continue
+            if visited[nx]:
+                continue
+
+            if nx == 2*x:
+                visited[nx] = visited[x]
+            else:
+                visited[nx] = visited[x] +1
+
+            q.append(nx)
+
+
+N, K = map(int,input().split())
+MAX = 10**5
+visited = [0] *(MAX+1)
+bfs(N,K)
+
+print(visited[K]-1)
